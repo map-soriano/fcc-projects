e62f74ec8bacbfcd335e0ed961522a78c4818476
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-07 22:25:39 +0800

    Include author information in generated quote

--- front-end-development-libraries/random-quote-machine/src/components/QuoteBox.tsx
+++ front-end-development-libraries/random-quote-machine/src/components/QuoteBox.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const QuoteBox = () => {
   const [quote, setQuote] = useState([]);
+  const [author, setAuthor] = useState([]);
 
   const num = Math.floor(Math.random() * 16);
 
@@ -9,7 +10,10 @@ const QuoteBox = () => {
     // fetch("https://animechan.xyz/api/random")
     fetch("https://type.fit/api/quotes")
       .then((res) => res.json())
-      .then((quote) => setQuote(quote[num].text));
+      .then((quote) => {
+        setQuote(quote[num].text);
+        setAuthor(quote[num].author);
+      });
   }, []);
 
   console.log(quote);
@@ -19,7 +23,7 @@ const QuoteBox = () => {
         {quote}
       </h1>
       <p id="author" className="text-right">
-        - Quote Author
+        - {author}
       </p>
       <button id="tweet-quote" className="btn btn-block btn-primary col-4">
         Tweet
